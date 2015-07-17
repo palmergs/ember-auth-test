a3df8a224a6e89a8f117e6499bc6b00fcb23b554
Galen Palmer <palmergs@gmail.com>
2015-07-16 20:11:57 -0400

    add loading templates

--- rarwe/app/controllers/bands/band/songs.js
+++ rarwe/app/controllers/bands/band/songs.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  needs: ['band'],
   queryParams: {
     sortBy: 'sort',
     searchTerm: 's',
--- rarwe/app/routes/bands.js
+++ rarwe/app/routes/bands.js
@@ -1,8 +1,19 @@
 import Ember from 'ember';
 
+function wait(promise, delay) {
+  return new Ember.RSVP.Promise(function(resolve) {
+    setTimeout(function() {
+      promise.then(function(result) {
+        resolve(result);
+      });
+    }, delay);
+  });
+}
+
 export default Ember.Route.extend({
   model: function() { 
-    return this.store.findAll('band');
+    var bands = this.store.findAll('band');
+    return wait(bands, 3 * 1000);
   },
   actions: {
     createBand: function() {
